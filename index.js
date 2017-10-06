e6f908745c1d629958fbe4052f0ddf79b9f3a1c7
park <ruden91@naver.com>
2017-10-06 20:36:31 +0900

    refactoring route index.js and install nodemon

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-// server side common.js style
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-// https://console.developers.google.com
-passport.use(
-	new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, (accessToken) => {
-		console.log(accessToken);
-	})
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-	scope: ['profile', 'email']
-}));
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
