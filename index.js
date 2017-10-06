e3cce6c26fd0ba944918d3c0c0025ee6c77baf5d
park <ruden91@naver.com>
2017-10-06 17:12:26 +0900

    initial setting with heroku

--- index.js
+++ index.js
@@ -8,12 +8,5 @@ app.get('/', (req, res) => {
 	});
 });
 
-
-app.get('/greeting', (req, res) => {
-	res.send({
-		hi: 'greeting'
-	});
-});
-
-
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
