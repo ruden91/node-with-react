601bbb134b81384079388bad60dd1591a681e0f1
park <ruden91@naver.com>
2017-10-06 19:26:45 +0900

    add passport.js and google+ oauth

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // server side common.js style
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({
-		bye: 'buddy'
-	});
-});
+// https://console.developers.google.com
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
